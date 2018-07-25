602cf8bfc0afbadb935fbbea84210cb78056167f
MCPhillips19 <mpclaire72@gmail.com>
2018-07-25 18:50:27 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -12,25 +12,28 @@ def first_student_by_index
 end
 
 def fourth_student_by_index
-  puts STUDENT_NAMES[3]
+   STUDENT_NAMES[3]
 end
 
 def last_student_by_index
-  puts STUDENT_NAMES[5]
+   STUDENT_NAMES[5]
 end
 
 def first_student_by_method
-  puts STUDENT_NAMES.first
+   STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  puts STUDENT_NAMES.last
+   STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-   puts STUDENT_NAMES[0,1,2]
+    STUDENT_NAMES[0,1,2]
 end
 first_student_by_index
 fourth_student_by_index
 last_student_by_index
+first_student_by_method
+last_student_by_method
+first_second_and_third_students
 
